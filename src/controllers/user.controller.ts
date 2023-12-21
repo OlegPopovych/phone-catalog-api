e701146708f2954576211ccc716d9a249b9ab87a
Oleh <olezhkapopovych@gmail.com>
2023-12-21 15:26:12 +0200

    changed get/orders object

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 'use strict';
 import { ControllerAction, User } from '../types';
 import * as userService from '../services/user.service';
+import { ProductsModel } from '../models';
 
 export const getCart: ControllerAction = async (req, res) => {
   const userId = (req.user as User).id;
@@ -73,15 +74,58 @@ export const getOrders: ControllerAction = async (req, res) => {
   const userId = (req.user as User).id;
 
   // try {
-  const orders = await userService.getOrders(userId);
+  const ordersFromDb = await userService.getOrders(userId);
 
-  if(orders === null) {
+  const parsedOrders = ordersFromDb.map(el => {
+    return {
+      id: el.id,
+      createdAt: el.createdAt,
+      order: JSON.parse(el.order),
+    };
+  });
+
+  const preparedOrders = [];
+
+  for (const orderItem of parsedOrders) {
+
+    const getData = async () => {
+      const prods = [];
+      for await (const prod of orderItem.order) {
+        const prodData = await ProductsModel.findOne({
+          where: {
+            itemId: prod.name,
+          }
+        });
+
+        prods.push({
+          quantity: prod.quantity,
+          title: prodData?.name,
+          color: prodData?.color,
+          imageUrl: prodData?.image,
+          price: prodData?.price,
+          fullPrice: prodData?.fullPrice,
+          itemId: prodData?.itemId,
+        });
+      }
+      return prods;
+    };
+
+    preparedOrders.push({
+      id: orderItem.id,
+      createdAt: orderItem.createdAt,
+      products: await getData(),
+    });
+  }
+
+  console.log(preparedOrders);
+
+  if(ordersFromDb === null) {
     res.send(JSON.stringify([]));
 
     return;
   }
 
-  res.send(orders);
+  res.send(preparedOrders);
   // } catch (error) {
   //   res.sendStatus(500);
   // }
@@ -102,3 +146,16 @@ export const createOrder: ControllerAction = async (req, res) => {
   //   res.sendStatus(500);
   // }
 };
+
+// [
+// 	{
+// 		createdAt,
+// 		products: [
+// 			{
+// 				name,
+// 				count,
+// 				img,
+// 			}
+// 		]
+// 	}
+// ]
