f6a3a3cd355609d2931d5542d0d77c9f5583f095
Oleh <olezhkapopovych@gmail.com>
2023-12-21 02:55:16 +0200

    comment

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -5,6 +5,5 @@ import { Request, Response } from 'express';
 export const errorHandler = (err: Error, req: Request, res: Response) => {
   console.error(err.stack);
 
-  // Повертаємо клієнту відповідь з кодом помилки та повідомленням
   res.status(500).send('Something went wrong!');
 };
